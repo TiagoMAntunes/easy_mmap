dd297fd5c95b4b6bffbbaceb657e6e689c58c4f3
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-08-24 20:48:42 +0100

    Better interface, boundary handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,70 @@
-use std::ops::Index;
+use std::marker::PhantomData;
 
 use mmap::{MapOption, MemoryMap};
 
-struct EasyMap {
+pub struct EasyMap {
     map: MemoryMap,
     capacity: usize,
 }
 
-struct EasyMapIter<'a, T> {
+pub struct EasyMapIter<'a, T> {
     map: &'a EasyMap,
     index: usize,
-    _v: T, // Hack to hold the type parameter
+    phantom: PhantomData<T>,
 }
 
 impl<'a> EasyMap {
-    pub fn new<T: Sized>(capacity: usize) -> EasyMap {
+    /// Creates a new EasyMap struct with enough capacity to hold AT LEAST `capacity` elements of type `T`.
+    pub fn new<T: Sized>(capacity: usize, options: &[MapOption]) -> EasyMap {
         EasyMap {
-            map: MemoryMap::new(
-                capacity * std::mem::size_of::<T>(),
-                &[MapOption::MapReadable, MapOption::MapWritable],
-            )
-            .unwrap(),
+            map: MemoryMap::new(capacity * std::mem::size_of::<T>(), options).unwrap(),
             capacity: capacity * std::mem::size_of::<T>(),
         }
     }
 
-    fn put<T>(&mut self, offset: usize, value: T) {
+    /// Inserts a value at index `index` in the EasyMap according to the type of `T`.
+    fn put<T>(&mut self, index: usize, value: T) {
+        if index >= self.len::<T>() {
+            panic!(
+                "Index {} is out of bounds for type {}",
+                index,
+                std::any::type_name::<T>(),
+            );
+        }
+
         unsafe {
             self.map
                 .data()
                 .cast::<T>()
-                .offset(offset as isize)
+                .offset(index as isize)
                 .write(value);
         }
     }
 
-    pub fn get<T>(&self, offset: usize) -> T {
-        unsafe { self.map.data().cast::<T>().offset(offset as isize).read() }
+    /// Gets a value at index `index` in the EasyMap according to the type of `T`.
+    pub fn get<T>(&self, index: usize) -> T {
+        if index >= self.len::<T>() {
+            panic!(
+                "Index {} is out of bounds for type {}",
+                index,
+                std::any::type_name::<T>(),
+            );
+        }
+
+        unsafe { self.map.data().cast::<T>().offset(index as isize).read() }
     }
 
+    /// Returns how many elements of type `T` fit in the `EasyMap`.
     pub fn len<T>(&self) -> usize {
         self.capacity / std::mem::size_of::<T>()
     }
 
+    /// Returns an iterator over `EasyMap` looking at the data as the type `T`.
     pub fn iter<T: Default>(&'a self) -> EasyMapIter<'a, T> {
         EasyMapIter::<T> {
             map: self,
             index: 0,
-            _v: Default::default(),
+            phantom: PhantomData,
         }
     }
 }
@@ -72,7 +89,7 @@ mod tests {
 
     #[test]
     fn map_create() {
-        let map = EasyMap::new::<u32>(10);
+        let map = EasyMap::new::<u32>(10, &[]);
 
         assert_eq!(map.len::<u32>(), 10);
         assert_eq!(map.len::<u64>(), 5);
@@ -80,7 +97,7 @@ mod tests {
 
     #[test]
     fn map_write_read() {
-        let map = &mut EasyMap::new::<u32>(1);
+        let map = &mut EasyMap::new::<u32>(1, &[MapOption::MapReadable, MapOption::MapWritable]);
         map.put::<u32>(0, 1);
 
         assert_eq!(map.get::<u32>(0), 1);
@@ -88,11 +105,27 @@ mod tests {
 
     #[test]
     fn map_iter() {
-        let map = &mut EasyMap::new::<u32>(5);
+        let map = &mut EasyMap::new::<u32>(5, &[MapOption::MapReadable, MapOption::MapWritable]);
         for i in 0..5 {
             map.put::<u32>(i, i as u32);
         }
 
         assert_eq!(map.iter::<u32>().collect::<Vec<_>>(), vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn map_oob_write() {
+        let map = &mut EasyMap::new::<u32>(1, &[MapOption::MapReadable, MapOption::MapWritable]);
+
+        map.put::<u32>(1, 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn map_oob_read() {
+        let map = &mut EasyMap::new::<u32>(1, &[MapOption::MapReadable, MapOption::MapWritable]);
+
+        map.get::<u32>(1);
     }
 }
